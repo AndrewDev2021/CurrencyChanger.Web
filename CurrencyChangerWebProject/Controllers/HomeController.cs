1781e638a58784745581ea2ef71d8604c4d742e6
Andrii Poberezhnik <andrepoper2016@gmail.com>
2022-05-15 23:18:39 +0300

    Add some UI refactoring,added the exchange rate from the National Bank's API Add currency rate view.

--- CurrencyChangerWebProject/Controllers/HomeController.cs
+++ CurrencyChangerWebProject/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
-﻿using CurrencyExсhanger.Web.Domain;
+﻿using System;
+using System.Collections.Generic;
+using CurrencyExсhanger.Web.Domain;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+using System.Threading.Tasks;
+using CurrencyExсhanger.Web.Model;
+using CurrencyExсhanger.Web.Services;
 using Microsoft.AspNetCore.Authorization;
 
 namespace CurrencyExсhanger.Web.Controllers
@@ -8,10 +13,12 @@ namespace CurrencyExсhanger.Web.Controllers
     public class HomeController : Controller
     {
         private AppDbContext _context;
+        private CurrencyRateService _currencyRateService;
 
         public HomeController(AppDbContext context)
         {
             _context = context;
+            _currencyRateService = new CurrencyRateService();
         }
 
         [Route("/")]
@@ -24,10 +31,19 @@ namespace CurrencyExсhanger.Web.Controllers
         [Route("/show")]
         public IActionResult ShowUsers()
         {
-            var users = _context.Users.First();
+            var users = _context.Users.Select(u => u).ToList();
 
             return View(users);
 
+        }
+
+        [Authorize]
+        [Route("/rate")]
+        public async Task<IActionResult> CurrencyRate()
+        {
+            var listOfRates = await _currencyRateService.GetRatesAsync();
+            
+            return View(listOfRates);
         }
     }
 }
--- CurrencyChangerWebProject/Model/CurrencyRate.cs
+++ CurrencyChangerWebProject/Model/CurrencyRate.cs
@@ -1,16 +1,16 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace CurrencyExсhanger.Web.Model
 {
     public class CurrencyRate
     {
-        [JsonPropertyName("txt")]
-        public string Name { get; set; }
+        public string Txt { get; set; }
 
-        [JsonPropertyName("rate")]
         public decimal Rate { get; set; }
 
-        [JsonPropertyName("cc")]
-        public string CurrencyCode { get; set; }
+        public string Cc { get; set; }
+        
+        public string ExchangeDate { get; set; }
     }
 }
